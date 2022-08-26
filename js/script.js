c2d332615111bd5365c346998046c6fef2a09537
unknown <fabiohn20@gmail.com>
2022-08-26 16:56:04 -0300

    melhorias no´código

--- js/script.js
+++ js/script.js
@@ -1,15 +1,118 @@
-/*
-Faz uma lista de TODO
-Uma tabela Aonde vc tem um form e vc preenche os dados e joga na tabela e ela se atualiza
+let modal = document.querySelector('.modal');
+let button = document.createElement('button');
+button.innerHTML = 'Voltar';
 
-Vc edita o dado é ela se atualiza
+class Lista {
+    constructor(descricao, tipo, ano, mes, dia) {
+        this.descricao = descricao;
+        this.tipo = tipo;
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+    }
+}
 
-Vc exclui o dado e ela se atualiza
+class Bd {
+    constructor() {
+        let id = localStorage.getItem('id');
 
-Sem dar refresh na página
-*/
+        if(id === null) {
+            id = localStorage.setItem('id', 0);
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id');
+        return parseInt(proximoId) + 1;
+    }
+
+    gravar(l) {
+        let id = this.getProximoId();
+
+        localStorage.setItem(id, JSON.stringify(l));
+
+        localStorage.setItem('id', id);
+    }
+
+    recuperarLista() {
+        localStorage.getItem();
+    }
+}
+
+let bd = new Bd();
+
+function cadastrarItem() {
+    let descricao = document.getElementById('descricao');
+    let tipo = document.getElementById('tipo');
+    let ano = document.getElementById('ano');
+    let mes = document.getElementById('mes');
+    let dia = document.getElementById('dia');
+
+    let lista = new Lista(
+        descricao.value,
+        tipo.value,
+        ano.value,
+        mes.value,
+        dia.value
+    );
+
+    button.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    if(descricao.value !== '' && tipo.value !== '' && ano.value !== '' && mes.value !== '' && dia.value !== '') {
+        bd.gravar(lista);
+        console.log(lista);
+
+        descricao.value = '';
+        tipo.value = '';
+        ano.value = '';
+        mes.value = '';
+        dia.value = '';
+
+        modal.style.display = 'block';
+        modal.innerHTML = '<h2>Item cadastrado com sucesso.</h2>';
+        modal.style.background = 'green';
+        button.className = 'btn btn-success';
+        modal.appendChild(button);
+
+        
+    } else {
+        modal.style.display = 'block';
+        modal.innerHTML = '<h3>Algo deu errado!</h3> <p>Verifique se todos os campos foram preenchidos.</p>';
+        modal.style.background = 'red';
+        button.className = 'btn btn-danger';
+        modal.appendChild(button);
+
+    }
+}
 
 
+function carregarLista() {
+    bd.recuperarLista();
+    // faltar fazer a lógica para carregar a lista na página de consulta
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* CÓDIGO ANTIGO
 
 let listaTodos = Array()
 listaTodos = []
@@ -60,3 +163,4 @@ function closeModal() {
     modal.style.display = 'none'
     document.querySelector('body').style.background = 'white'
 }
+*/
